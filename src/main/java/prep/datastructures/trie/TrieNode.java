a7c274e95982dfbaaf57a93251790bf020b98d41
Rob Bart <rbart234@gmail.com>
2015-04-28 16:28:42 -0700

    Add TrieNode.

---
+++ src/main/java/prep/datastructures/trie/TrieNode.java
@@ -0,0 +1,37 @@
+package prep.datastructures.trie;
+
+public class TrieNode {
+
+	public TrieNode[] children;
+	public int numChildren;
+	public boolean isWord;
+	
+	public TrieNode() {
+		children = new TrieNode[Trie.ALPHABET_SIZE];
+		numChildren = 0;
+		isWord = false;
+	}
+	
+	public boolean isLeaf() {
+		return numChildren == 0;
+	}
+	
+	public void addNewChild(char c, TrieNode child) {
+		int index = Trie.charToIndex(c);
+		if (children[index] != null) {
+			throw new RuntimeException("TrieNode already has child for char '"+c+"'");
+		}
+		children[index] = child;
+	}
+	
+	public TrieNode getChild(char c) {
+		return children[Trie.charToIndex(c)];
+	}
+	
+	public int firstChildIndex() {
+		for (int i = 0; i < Trie.ALPHABET_SIZE; i++) {
+			if (children[i] != null) return i;
+		}
+		throw new RuntimeException("Node has no children.");
+	}
+}
